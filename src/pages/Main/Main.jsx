84c51e78032795a4ea91854cff3a7de223c8c7c4
hamxxn <hamin031121@gmail.com>
2025-01-02 23:33:47 +0900

    main page 게시글 ing

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,10 +1,16 @@
+import MainImg from "../../assets/icons/main.svg";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
-
+import Contents from "../../components/Contents/Contents";
+import { Img } from "./Main.styles";
 export default function Main() {
   return (
     <>
       <Header />
+      <Img>
+        <img src={MainImg} alt="main img" />
+      </Img>
+      <Contents />
       <Footer />
     </>
   );
