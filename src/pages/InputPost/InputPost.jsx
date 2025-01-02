b234a8d24f8692cf7f5c83730b01cab04c4eb1ef
heeyongKim <166043860+heeeeyong@users.noreply.github.com>
2025-01-02 23:03:08 +0900

    기초작업

---
+++ src/pages/InputPost/InputPost.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import InputContext from '../../components/InputContext/InputContext';
+
+const InputPost = () => {
+  return (
+    <>
+    <InputContext />
+    </>
+  )
+};
+
+export default InputPost;
---
+++ src/pages/OutputPost/OutputPost.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const OutputPost = () => {
+  return (
+    <div>OuputPost</div>
+  )
+};
+
+export default OutputPost;
