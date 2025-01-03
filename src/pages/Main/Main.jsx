c999b574dc94a997f311068bac67c3a288d568c4
heeyongKim <166043860+heeeeyong@users.noreply.github.com>
2025-01-03 21:14:47 +0900

    loginpopup

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import MainImg from "../../assets/icons/main.svg";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
@@ -5,11 +6,17 @@ import Contents from "../../components/Contents/Contents";
 import { Img } from "./Main.styles";
 import Comments from "../../components/Comments/Comments";
 import Button from "../../components/Button/Button";
+import LoginPopup from "../../components/LoginPopup/LoginPopup";
 export default function Main() {
+
+  const [onModal, setOnModal] = useState(false); // 모달 상태 관리
+  
   return (
     <>
+      {onModal && <LoginPopup setOnModal={setOnModal} />}
+
       <Button />
-      <Header />
+      <Header setOnModal={setOnModal} />
       <Img>
         <img src={MainImg} alt="main img" />
       </Img>
